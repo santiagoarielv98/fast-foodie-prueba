8c8bfa547e2f500691ebb1996d76ab2131021e85
Santiago Ariel <santiagoarielv98@gmail.com>
2024-05-03 01:09:33 -0300

    cambios

--- src/app/api/reserve/route.ts
+++ src/app/api/reserve/route.ts
@@ -1,7 +1,18 @@
 import { ConfirmationSchemaValues } from "@/app/reserve/schemas/confirmation-schema";
+import {} from "mongodb";
 
 import clientPromise from "@/lib/mongodb";
 
+export async function GET() {
+  const client = await clientPromise;
+
+  const db = client.db();
+
+  const collection = db.collection("reservations");
+  const res = await collection.find().toArray();
+
+  return Response.json(res);
+}
 export async function POST(request: Request) {
   const res: ConfirmationSchemaValues = await request.json();
   const client = await clientPromise;
@@ -12,5 +23,5 @@ export async function POST(request: Request) {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   await collection.insertOne(res);
 
-  return Response.json({ res });
+  return Response.json(res);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,45 @@
 import React from "react";
+import { ConfirmationSchemaValues } from "./reserve/schemas/confirmation-schema";
+import { WithId } from "mongodb";
+import { Button, Table } from "react-bootstrap";
 
-export default function Home() {
-  return <div>Home</div>;
+export default async function Home() {
+  const res = await fetch("http://localhost:3000/api/reserve");
+  const data: WithId<ConfirmationSchemaValues>[] = await res.json();
+
+  return (
+    <div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item._id.toString()} className="align-middle">
+              <td>{item.name}</td>
+              <td>{item.lastName}</td>
+              <td>{item.email}</td>
+              <td>{item.date}</td>
+              <td>{item.time}</td>
+              <td className="d-flex gap-2">
+                <Button variant="danger">
+                  <i className="bi bi-trash"></i>
+                </Button>
+                <Button variant="primary">
+                  <i className="bi bi-pencil"></i>
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 }
